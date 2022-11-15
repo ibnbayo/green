8ccc21f23435704b03ac7ea0e780024401fc67ec
AF <100952220+ibnbayo@users.noreply.github.com>
2022-11-15 13:29:42 +0100

    pass trading pairs data

---
+++ components/layout/MainNavigation.js
@@ -0,0 +1,15 @@
+import Link from 'next/link'
+import classes from './MainNavigation.module.css';
+
+function MainNavigation() {
+
+  return (
+    <header className={classes.header}>
+      <div className={classes.logo}>Green</div>
+      <nav>
+      </nav>
+    </header>
+  );
+}
+
+export default MainNavigation;
